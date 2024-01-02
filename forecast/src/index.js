cda1e83045329ed35031083bc1d334aef3f69519
OlenaBarylova <149913475+OlenaBarylova@users.noreply.github.com>
2024-01-02 02:58:57 +0200

    Update index.js

--- forecast/src/index.js
+++ forecast/src/index.js
@@ -98,5 +98,4 @@ function displayForecast(response) {
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
-
 searchCity("Kyiv");
